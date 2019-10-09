243be1bfd84b1330ef467a4a43998ab2f499fa3d
Chantel Lowe <chantellowe17@gmail.com>
2019-10-09 05:54:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,15 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    
+    doc = Nokogiri::HTML(html)
+    
+    #name: doc.css("h4.student-name").text
+    #location: doc.css("p.student-location").text
+    #profile_url: doc.css(".student-card a").attribute("href").value
+    
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
